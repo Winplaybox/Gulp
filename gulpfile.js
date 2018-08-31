62a4e7f4a68e9de7bdf1aeb2b096454fe64f2f64
Sagar Mistry <sagar.mistry@pdc.com>
2018-08-31 16:59:22 +0530

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+// logs Message
+
+gulp.task('message',function(){
+    return console.log('Gulp is running...');
+  });
+
+// copying all html files
+
+gulp.task('copyHTML',function(){
+    gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+  }); 
+
+// optimize images
+
+gulp.task('imagemin',function(){
+gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+});
+
 
 gulp.task('sass',function(){
     gulp.src('src/sass/*.scss')
